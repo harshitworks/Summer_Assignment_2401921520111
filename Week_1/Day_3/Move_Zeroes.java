9d949fd6487cb4fc12414b54c16ed6b8c7f3fdd9
Harshit Gaur <gaurharshit480@gmail.com>
2026-06-03 10:45:18 +0530

    Solved Move Zeroes problem

---
+++ Week_1/Day_3/Move_Zeroes.java
@@ -0,0 +1,21 @@
+package Week_1.Day_3;
+
+public class Move_Zeroes {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int l = 0 ;
+        int k = 0  ;
+        // shifting the non-zero babies forward
+        while(l < n) {
+            if(nums[l]!=0) {
+                nums[k++]=nums[l];
+            }
+            l++;
+        }
+        // filling the last cell with 0
+        while(k<n) {
+            nums[k++]=0;
+        }
+
+    }
+}
