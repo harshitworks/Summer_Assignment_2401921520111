8fc9f0d19a4b74e86dcaeb9e20672cf5daadf17f
Harshit Gaur <gaurharshit480@gmail.com>
2026-06-16 13:24:25 +0530

    Add solution to LC 206: Reverse Linked List problem

---
+++ Week_3/Day_1/Reverse_LinkedList.java
@@ -0,0 +1,20 @@
+package Week_3.Day_1;
+
+
+
+
+class Reverse_LinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+}
